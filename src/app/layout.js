e93b50e57b2d9de90bde43bf6c1ec6108642f359
RAFEL <rmallqui@gmail.com>
2023-07-17 02:58:55 -0300

    cambios

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
+import Footer from '@/componets/@Footer/Footer'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Nav from '@/componets/@Nav/Nav'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Nav />
+        <div> 
+          {children}
+        </div>
+        <Footer />
+
+        </body>
     </html>
   )
 }
---
+++ src/componets/@Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <nav className="bg-blue-500">
+        <ul className="flex justify-between">
+            <li className="p-4">Home</li>
+            <li className="p-4">About</li>
+            <li className="p-4">Contact</li>
+        </ul>
+            
+    </nav>
+  )
+}
